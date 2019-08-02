eb284cf3c6ac720b02659559c27eba742c2be3b8
Martin Hickey <martin.hickey@ie.ibm.com>
2019-08-02 10:45:15 +0100

    Add files via upload

--- tool/helm2to3/pkg/helm2to3/v2/release.go
+++ tool/helm2to3/pkg/helm2to3/v2/release.go
@@ -3,12 +3,13 @@ package v2
 import (
 	"k8s.io/helm/pkg/helm"
         "k8s.io/helm/pkg/proto/hapi/release"
+	rls "k8s.io/helm/pkg/proto/hapi/services"
 )
 
 func GetRelease(releaseName string) (*release.Release, error) {
-        helmClient, err := GetHelmClient()
+	helmClient, err := GetHelmClient()
 	if err != nil {
-	       return nil, err
+	        return nil, err
         }
 
 	res, err := helmClient.ReleaseContent(releaseName, helm.ContentReleaseVersion(1))
@@ -17,4 +18,40 @@ func GetRelease(releaseName string) (*release.Release, error) {
         }
 
 	return res.Release, nil
+}
+
+func GetReleaseVersions(releaseName string) ([]*release.Release, error) {
+	helmClient, err := GetHelmClient()
+        if err != nil {
+                return nil, err
+        }
+
+	r, err := helmClient.ReleaseHistory(releaseName, helm.WithMaxHistory(histMax))
+        if err != nil {
+                return nil, err
+        }
+
+	return r.Releases, nil
+}
+
+func DeleteRelease(releaseName string) (*rls.UninstallReleaseResponse, error) {
+        opts := []helm.DeleteOption{
+		 helm.DeleteDryRun(false),
+                helm.DeleteDisableHooks(false),
+                helm.DeletePurge(true),
+                helm.DeleteTimeout(300),
+                helm.DeleteDescription(""),
+        }
+
+	helmClient, err := GetHelmClient()
+        if err != nil {
+                return nil, err
+        }
+
+        resp, err := helmClient.DeleteRelease(releaseName, opts...)
+        if err != nil {
+                return nil, err
+        }
+
+        return resp, nil
 }
